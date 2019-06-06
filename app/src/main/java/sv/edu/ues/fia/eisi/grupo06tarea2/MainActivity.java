fcfb095e6f2beac72fa8af0adb78a174ef0e2980
Maby <mabigailg@hotmail.com>
2019-06-05 23:06:06 -0600

    Iniciando la pantalla principal

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
@@ -1,7 +1,9 @@
 package sv.edu.ues.fia.eisi.grupo06tarea2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void iniciarPedido(View v)
+    {
+        Intent ints = new Intent(this,MenuPrincipal.class);
+        startActivity(ints);
+
+
     }
 }
