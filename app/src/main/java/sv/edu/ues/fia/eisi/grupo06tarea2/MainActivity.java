bb42ad66afd9850c7f36cf560936dbe5a345336e
Maby <mabigailg@hotmail.com>
2019-06-06 22:11:05 -0600

    Agregando la libreria GLIDE y colocando gifts para utilizarla

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView fotoImagenView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fotoImagenView = (ImageView) findViewById(R.id.imagenInicio);
+
+        String url="https://media1.tenor.com/images/1bc8809ba3844ca9638f37e11d2fe172/tenor.gif?itemid=9138694";
+
+        Glide.with(this)
+                .load(url)
+                .into(fotoImagenView);
     }
 
     public  void iniciarPedido(View v)
--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MenuPrincipal.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MenuPrincipal.java
@@ -2,12 +2,22 @@ package sv.edu.ues.fia.eisi.grupo06tarea2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 public class MenuPrincipal extends AppCompatActivity {
-
+private ImageView imagenPedido;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        imagenPedido = (ImageView) findViewById(R.id.imagenPedido);
+
+        String url="https://prodimages.restaurants-sign.com/350/l717865-pupusas-animated-led-sign.gif";
+
+        Glide.with(this)
+                .load(url)
+                .into(imagenPedido);
+
     }
 }
