10a739140d6475b488f2434600c02947933a46f4
Maby <mabigailg@hotmail.com>
2019-06-08 11:19:06 -0600

    Agreando el codigo para la notificacion PUSH

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MainActivity.java
@@ -1,18 +1,28 @@
 package sv.edu.ues.fia.eisi.grupo06tarea2;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //Variable utilizada en la libreria GLIDE
     private ImageView fotoImagenView;
+    //Variables para la notificacion PUSH
+    NotificationCompat.Builder mBuilder;
+    int mNotificationId=001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //***************Codigo para utilizar la libreria GLIDE******************
         fotoImagenView = (ImageView) findViewById(R.id.imagenInicio);
 
         String url="https://media1.tenor.com/images/1bc8809ba3844ca9638f37e11d2fe172/tenor.gif?itemid=9138694";
@@ -20,6 +30,28 @@ public class MainActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(url)
                 .into(fotoImagenView);
+
+        //**************Codigo para utilizar la notificacion PUSH************************
+        mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.informacion)
+                        .setContentTitle("INFORMACIÓN")
+                        .setContentText("Todo lo que debes saber sobre nosotros");
+        // Activity que se lanza al hacer click a la notificacion
+        Intent resultIntent=new Intent(this, ResultActivity.class);
+        PendingIntent resultPendingIntent=
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotifyMgr=
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        mNotifyMgr.notify(mNotificationId,mBuilder.build());
     }
 
     public  void iniciarPedido(View v)
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/ResultActivity.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.fia.eisi.grupo06tarea2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
