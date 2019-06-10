f1212607780b0177a104ac3832f05ffd6c7c164f
chesterven <vd16006@ues.edu.sv>
2019-06-09 19:40:59 -0600

    Libreria volley guardar orden y detalle de pedido

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/CrearPedidos.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/CrearPedidos.java
@@ -1,13 +1,189 @@
 package sv.edu.ues.fia.eisi.grupo06tarea2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CrearPedidos extends AppCompatActivity {
+    Spinner pupas,masa;
+    EditText cantidad;
+    ListView lista;
+    //Almacenar el id de la orden de la pantalla anterior
+    String idOrden;
+
+
+    ArrayList<String> stringEspecialidades  = new ArrayList<String>();
+    ArrayList<String> masas = new ArrayList<String>();
+    ArrayList<String> pedidoLista = new ArrayList<String>();
+    RequestQueue requestQueue;
+    String URL="http://192.168.1.12:80/pupasWeb/mostrarEspecialidades.php/";
+    ArrayAdapter<String> adaptadorList;
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_pedidos);
+        Intent recibir = getIntent();
+        idOrden = recibir.getStringExtra("idOrden");
+
+        pupas = (Spinner) findViewById(R.id.pupa);
+        masa = (Spinner) findViewById(R.id.masa);
+        cantidad = (EditText) findViewById(R.id.editCantidad);
+        lista = (ListView) findViewById(R.id.listaPedido);
+
+        masas.add("Seleccione tipo de masa");
+        masas.add("Maiz");
+        masas.add("Arroz");
+        ArrayAdapter<CharSequence> adaptadorMasas = new ArrayAdapter(this,android.R.layout.simple_spinner_item,masas);
+        masa.setAdapter(adaptadorMasas);
+
+        loadEspecialidades();
+
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int posicion=i;
+
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(CrearPedidos.this);
+                dialogo1.setTitle("Importante");
+                dialogo1.setMessage("¿ Eliminara este pedido?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        pedidoLista.remove(posicion);
+                        adaptadorList.notifyDataSetChanged();
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                    }
+                });
+                dialogo1.show();
+
+                return false;
+            }
+        });
+    }
+
+    public void guardarPedido(View view) {
+        for(int x=0; x<pedidoLista.size();x++) {
+            ejecutarServicio("http://192.168.1.12:80/pupasWeb/insertarPedido.php/");
+        }
+
+
+        Toast.makeText(this, "Guardado", Toast.LENGTH_SHORT).show();
+    }
+
+    public void agregarPedido(View view) {
+        String eleccion = pupas.getSelectedItem().toString();
+        String[]partEleccion = eleccion.split(" ");
+        pedidoLista.add(eleccion+" "+cantidad.getText().toString()+" "+masa.getSelectedItem().toString());
+
+        adaptadorList = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,pedidoLista);
+        lista.setAdapter(adaptadorList);
+    }
+
+    private void loadEspecialidades(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray especialidades = new JSONArray(response);
+
+                    for(int i=0; i< especialidades.length();i++){
+                        JSONObject especialidadObject = especialidades.getJSONObject(i);
+
+                        int id = especialidadObject.getInt("id");
+                        String nombre = especialidadObject.getString("nombre");
+                        String especialidad = especialidadObject.getString("especialidad");
+                        double precio = especialidadObject.getDouble("precioUni");
+                        String photo = especialidadObject.getString("photo");
+                        stringEspecialidades.add(id+" "+especialidad+" "+precio);
+
+
+
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CrearPedidos.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+        stringEspecialidades.add(0,"Seleccione pupusa");
+        ArrayAdapter<CharSequence> adaptador = new ArrayAdapter(this,android.R.layout.simple_spinner_item,stringEspecialidades);
+        pupas.setAdapter(adaptador);
+
+    }
+
+    private void ejecutarServicio(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(CrearPedidos.this, "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CrearPedidos.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("order_id", idOrden);
+                String pedido = pedidoLista.get(i);
+                String[] partPedido = pedido.split(" ");
+
+
+                parametros.put("specialty_id",partPedido[partPedido.length-partPedido.length]);
+                parametros.put("cantidad", partPedido[partPedido.length-2]);
+                parametros.put("masa", partPedido[partPedido.length-1]);
+                i++;
+
+                return parametros;
+            }
+        };
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
     }
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MenuPrincipal.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/MenuPrincipal.java
@@ -4,20 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MenuPrincipal extends AppCompatActivity {
 private ImageView imagenPedido;
 private EditText nombreyApellido;
+private CheckBox llevar;
+    String id="";
+
+//Consulta en cola para webService
+RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
         imagenPedido = (ImageView) findViewById(R.id.imagenPedido);
         nombreyApellido = (EditText) findViewById(R.id.nombreyApellidoUsuario);
+        llevar = (CheckBox) findViewById(R.id.checkBoxLlevar);
 
 
         String url="https://prodimages.restaurants-sign.com/350/l717865-pupusas-animated-led-sign.gif";
@@ -34,8 +52,51 @@ private EditText nombreyApellido;
             Toast.makeText(this, "Debe ingresar su nombre y apellido", Toast.LENGTH_SHORT).show();
         }
         else {
-            Intent inte1 = new Intent(this, CrearPedidos.class);
-            startActivity(inte1);
+
+            ejecutarServicio("http://192.168.1.12:80/pupasWeb/insertarOrden.php/");
+
+
+
         }
+
+
+    }
+    
+    //Metodo para ingresar la orden 
+    private void ejecutarServicio(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MenuPrincipal.this, "OPERACION EXITOSA"+response, Toast.LENGTH_SHORT).show();
+                id=response;
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MenuPrincipal.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("nombre", nombreyApellido.getText().toString());
+                if(llevar.isChecked()){
+                    parametros.put("delivery", "1");
+                }else{
+                    parametros.put("delivery", "0");
+                }
+                return parametros;
+            }
+        };
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    public void pasarOtra(View view) {
+        Intent inte1 = new Intent(this, CrearPedidos.class);
+        inte1.putExtra("idOrden",id);
+        startActivity(inte1);
     }
 }
