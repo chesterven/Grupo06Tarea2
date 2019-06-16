45f96334d64b2227afdbf383ac7a83f6b1c190cd
ariezelaya <cristianarielzelaya@hotmail.com>
2019-06-16 02:45:16 -0600

    Descuentos2

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/DescuentosCamara.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea2/DescuentosCamara.java
@@ -109,6 +109,6 @@ public class DescuentosCamara extends AppCompatActivity {
         Uri uri = Uri.fromFile(imageFileToShare);
         share.putExtra(Intent.EXTRA_STREAM, uri);
         share.putExtra(Intent.EXTRA_TEXT,"Con pupas ya me la paso rico!!");
-        startActivity(Intent.createChooser(share, "Share Image!"));
+        startActivity(Intent.createChooser(share, "Comparte en redes!"));
     }
 }
